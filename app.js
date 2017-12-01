8773a7420e20d61e683954eebca56d3635f0331f
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-01 14:33:28 -0700

    Added 2nd index file and infinity function to js

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 'use strict';
 
 
-function whoAmI(name) {
-  console.log('Hi, my  name is Jeff and I\'m 26 years old'); 
+// function whoAmI(name) {
+//   console.log('Hi, my  name is Jeff and I\'m 26 years old'); 
+// }
+
+// function yearOfBirth(age){
+//   try {
+//     if (age < 0) throw 'is a negative number';
+//   }
+//   catch (e){
+//     console.log('Enter a positive number for your age');
+//   }
+//   // if (age < 0){
+//   //   throw new Error('Must provide a positive number for your age');
+//   // }
+//   let yearOfBirthCalc = 2017 - age;  
+//   console.log('I was born in ' + yearOfBirthCalc);   
+// }
+
+// whoAmI();
+// yearOfBirth(-3);
+
+function jediName(firstName, lastName){
+  console.log(`${lastName.slice(0, 3)}${firstName.slice(0, 2)}`);
 }
 
-function yearOfBirth(age){
-  let yearOfBirthCalc = 2017 - age;  
-  console.log('I was born in ' + yearOfBirthCalc);   
+jediName('Jeff', 'Connelly');
+
+function beyond(num) {
+  if (num === Infinity){
+    console.log('And beyond');
+  }
+  else if (num < Infinity && num > 0) {
+    console.log('To infinity');
+  }
+  else if (num < Infinity && num < 0){
+    console.log('To negative infinity');
+  }
+  else {
+    console.log('Staying home');
+  }
 }
 
-whoAmI();
-yearOfBirth(26);
+beyond(-4);
+
