95ae03600becd371d816bf85ca7acf777a38beb1
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-01 11:22:39 -0700

    Added year of birth calculcation

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 'use strict';
 
 
-function whoAmI(name, age) {
-  let yearOfBirth = 2017 - age;
+function whoAmI(name) {
   console.log('Hi, my  name is Jeff and I\'m 26 years old'); 
-  console.log('I was born in ' + yearOfBirth); 
+}
+
+function yearOfBirth(age){
+  let yearOfBirthCalc = 2017 - age;  
+  console.log('I was born in ' + yearOfBirthCalc);   
 }
 
 whoAmI();
+yearOfBirth(26);
